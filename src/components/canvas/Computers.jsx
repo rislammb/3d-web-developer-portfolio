768d6a1134ae798c8d888718447d0b0a43e0d9aa
unknown <rislammb@gmail.com>
2024-02-24 13:08:57 +0600

    hero section complete with 3d computer and loading message

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,28 +1,81 @@
-import { useGLTF } from '@react-three/drei';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
+
   return (
-    <Suspense fallback={<CanvasLoader />}>
-      <Canvas>
-        <mesh
-          scale={[0.35, 0.35, 0.35]}
-          position={[0, -1, 0]}
-          rotation={[0, -1.6, -0.2]}
-        >
-          <hemisphereLight intensity={0.15} groundColor={'black'} />
-          <pointLight intensity={1} />
-          {/* <directionalLight position={[-0, 3, 2]} intensity={0.9} />
-          <ambientLight intensity={0.8} /> */}
-          <primitive object={computer.scene} />
-        </mesh>
-      </Canvas>
-    </Suspense>
+    <mesh>
+      <hemisphereLight intensity={1.15} groundColor='black' />
+      <pointLight intensity={1.3} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <ambientLight intensity={1.5} />
+      <directionalLight intensity={1.5} />
+
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
   );
 };
 
-export default Computers;
+const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 640px)');
+
+    // Set the initial value of the 'isMobile' state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = () => {
+      console.log('start change');
+      setIsMobile(mediaQuery.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputerCanvas;
